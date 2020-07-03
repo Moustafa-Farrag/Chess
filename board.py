def0d9f69c2820dbe461efe1db5e5a0d83c50ce4
moustafa-Farrag <safy.mohamed533@gmail.com>
2020-07-03 21:39:14 +0200

    queeen , rook and ele

--- board.py
+++ board.py
@@ -1,30 +1,33 @@
+board = []
+selecMove = set()
+
 def main():
+
     rows, cols = (8,8)
     EasyForIf = { 1 : "p" , 2 : "r" , 3 : "h" , 4 : "b" , 5 : "K" , 6 : "q" , 0 : "E" }  
 
     arr1 = [0]*cols 
     solider = [1]*8;
-    arr = []
     for i in range(8):
-        arr.append(list(arr1))
+        board.append(list(arr1))
 
-    print (arr) 
-    arr[1] = list(solider)
-    arr[6] = list(solider)
+    #print (arr) 
+    board[1] = list(solider)
+    board[6] = list(solider)
 
     king = [0]*8 ; 
     for i in range(3):
         king[i] = king[7-i] = i+2
 
 
-    arr[0] = list(king)
-    arr[7] = list(king)
-    arr[7][3] = 6; 
-    arr[7][4] = 5; 
-    arr[0][3] = 6;
-    arr[0][4] = 5;
+    board[0] = list(king)
+    board[7] = list(king)
+    board[7][3] = 6; 
+    board[7][4] = 5; 
+    board[0][3] = 6;
+    board[0][4] = 5;
     for i in range(8):
-            print(str(arr[i]).replace("," , ""))
+           print(str(board[i]).replace("," , ""))
 
     order = input("enter order: ");
     order = order.split(':');
@@ -35,7 +38,6 @@ def main():
     elif order[0] == "help":
         print(EasyForIf)
 
-selecMove = set()
 
 def sKinght(x , y):
     newX = x - 2 
@@ -85,35 +87,77 @@ def sRook(x ,y):
     for i in range(8):
         if x+i > 7:
             break;
-        if board[x+i][y] != ' ':
-            #chek if color
-            break;
-        selecMov.append((x+i , y))
+       # if board[x+i][y] != ' ':
+       #     break;
+        selecMove.add((x+i , y))
     for i in range(8):
         if x-i < 0:
             break;
-        if board[x-i][y] != ' ':
+       # if board[x-i][y] != ' ':
             #chek if color
-            break;
-        selecmov.append((x-i , y))
+       #     break;
+        selecMove.add((x-i , y))
     for i in range(8):
         if y-i < 0:
             break;
-        if board[x][y-i] != ' ':
+       # if board[x][y-i] != ' ':
             #chek if color
-            break;
-        selecmov.append((x , y-i))
+       #     break;
+        selecMove.add((x , y-i))
     for i in range(8):
         if y+i > 7:
             break;
-        if board[x][y+i] != ' ':
+       # if board[x][y+i] != ' ':
             #chek if color
+       #     break;
+        selecMove.add((x , y+i))
+    print (selecMove) 
+    return 
+
+def sBishop(x ,y):
+    for i in range(8):
+        if y+i > 7 or x+i > 7:
             break;
-        selecmov.append((x , y+i))
+        #if board[x+i][y+i] != ' ':
+        #    break;
+        selecMove.add((x+i , y+i))
+
+    for i in range(8):
+        if y-i < 0 or x-i < 0:
+            break;
+        #if board[x-i][y-i] != ' ':
+            #chek if color
+        #    break;
+        selecMove.add((x-i , y-i))
+
+    for i in range(8):
+        if y+i > 7 or x-i < 0 :
+            break;
+        #if board[x-i][y+i] != ' ':
+            #chek if color
+        #    break;
+        selecMove.add((x-i , y+i))
+
+    for i in range(8):
+        if y-i < 0 or x+i > 7  :
+            break;
+        #if board[x+i][y-i] != ' ':
+        #    break;
+        selecMove.add((x+i , y-i))
+        #print('sssssssss')
+    
+    for i in selecMove:
+        board[i[0]][i[1]] = 9
+    print (selecMove) 
     return 
 
-def sBishop():
+def sQueen(x,y):
+    sRook(x,y) 
+    sBishop(x,y)
+    return
 
-    return 
-sKinght(1,2)
 main()
+sQueen(1 ,2)
+#sKinght(1,2)
+for i in range(8):
+    print(str(board[i]).replace("," , ""))
